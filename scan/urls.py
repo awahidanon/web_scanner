7d206ee9275116eb025ce1a8f361aa02669dee44
Abdulwahid <92226916+awahidanon@users.noreply.github.com>
2024-08-27 20:54:31 +0300

    template update

--- scan/urls.py
+++ scan/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('initiate_scan/', views.initiate_scan, name='initiate_scan'),
+    path('', views.initiate_scan, name='initiate_scan'),
     path('scan_results/', views.scan_results, name='scan_results'),
     path('scan_results/<int:scan_id>/', views.scan_results, name='scan_results_with_id'),
 ]
